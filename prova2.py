0f32873790c5c495e4bc70a78c7106eba46d7001
kandura <81191275+kandura@users.noreply.github.com>
2021-04-12 22:17:33 -0300

    Create prova2.py

---
+++ prova2.py
@@ -0,0 +1,17 @@
+def nota_do_aluno():
+  print("Média do aluno é:")
+  primeiranota = (input(7))
+  segundanota = (input(6))
+  terceiranota = (input(8))
+  return (primeiranota + segundanota + terceiranota) / 3
+nota_do_aluno()
+
+
+N = int(input(7))
+lista = [4,5,2,8,6,3,7]
+def funcao_da_lista():
+  i = 0
+  while i < N:
+    num = input(7)
+    
+    
